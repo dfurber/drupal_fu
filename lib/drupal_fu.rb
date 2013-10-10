dc6ed9698fe0e46a2438fe22ff1f687cc03c5e05
David Furber <furberd@gmail.com>
2013-10-09 21:30:39 -0400

    Update drupal_fu.rb

--- lib/drupal_fu.rb
+++ lib/drupal_fu.rb
@@ -1,2 +1,4 @@
-files = File.join File.dirname(__FILE__), 'drupal', '*'
-Dir.glob(files, &method(:require))
+%w{base helper authmap menu_link menu_router profile_field serialize 
+   session session_store url_alias user variable}.each do |file|
+  require "drupal/#{file}"
+end
